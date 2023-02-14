f368e79f1d0fc763b828dbcccf6f8d8babfe0fe0
shadowy-pycoder <shadowy-pycoder@example.com>
2023-02-14 18:49:56 +0300

    dealing with env variables

--- web/website/__init__.py
+++ web/website/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py', silent=True)
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['DEBUG'] = os.environ.get('DEBUG')
     from .views import views, page_not_found
     app.register_blueprint(views, url_prefix='/')
     app.register_error_handler(404, page_not_found)
